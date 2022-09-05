9938426a233ea50f12f728dbe9c7b14b80ddb03d
Jorge Rodríguez <>
2022-09-05 11:33:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+a=3
+b=4
+print('antes del cambio')
+print ('el valor de a es', a)
+print ('el valor de b es', b)
+temp=a    #guardamos el valor de la variable a en la    variable temp
+a=b       #asignamos a la variable a el valor de b
+b=temp     #asignamos a b el valor almacenado en temp
+print('después del cambio')
+print ('el valor de a es', a)
+print ('el valor de b es', b)
